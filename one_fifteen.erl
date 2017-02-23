b9c0478bb76101e31f149e5ca55746b82b69edeb
Nathan Sculli <scull7@gmail.com>
2017-02-22 19:01:04 -0800

    fix xor2, hopefully for real this time.

--- one_fifteen.erl
+++ one_fifteen.erl
@@ -1,12 +1,12 @@
 -module(one_fifteen).
 -export([xor1/2,xor2/2,xor3/2,maxThree/3,howManyEqual/3]).
--export([all/0,xor2_true_false/1]).
+-export([all/0,xor2_true_false/1,xor2_false_false/1]).
 
 xor1(X,Y) ->
 	X =/= Y.
 
 xor2(X,Y) ->
-	not (X and Y).
+	(X or Y) and (not (X and Y)).
 
 xor3(X,Y) ->
 	not X == Y.
@@ -36,7 +36,7 @@ howManyEqual(_,_,_) ->
 
 %% Common Test, Test Suite
 
-all() -> [xor2_true_false].
+all() -> [xor2_true_false,xor2_false_false].
 
 
 xor2_true_false(_Config) ->
@@ -44,3 +44,11 @@ xor2_true_false(_Config) ->
 		true -> ok;
 		X -> ct:fail({xor2_true_false_failed, X})
 	end.
+
+
+xor2_false_false(_Config) ->
+	case xor2(false,false) of
+		false -> ok;
+		X -> ct:fail({xor2_false_false_failed, X})
+	end.
+
